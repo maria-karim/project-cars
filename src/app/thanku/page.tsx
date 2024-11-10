eb7ab1fdcef8d487ab8ea5566883accd2f9255e7
maria-karim <167464098+maria-karim@users.noreply.github.com>
2024-11-11 00:31:29 +0500

    Add files via upload

---
+++ src/app/thanku/page.tsx
@@ -0,0 +1,16 @@
+
+export default function ThankYouMessage() {
+  return (
+      
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-blue-600 font-bold text-5xl">
+        Thank you for shopping !!!
+      </h1>
+      <p className="text-black text-xl mt-2">
+        Your order will be delivered in 3 working days
+      </p>
+    </div>
+  );
+};
+
+
